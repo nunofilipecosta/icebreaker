afa95068fae51c24df52bbcd2c32f56b4045c56a
David Silva Cordeiro <19530208+davidscordeiro@users.noreply.github.com>
2020-05-09 00:07:55 +0100

    revert stuff that doesn't work on safari

--- functions.js
+++ functions.js
@@ -1,48 +1,22 @@
-(function (icebreaker, undefined) {
-    const USED_QUESTIONS_BUFFER_MAX_SIZE = 10;
 
-    class IceBreaker {
-        usedQuestionsBuffer = []
+function start(){
+    document.getElementById('start').style.display='none';
+    document.getElementById('play').style.display='flex';
+    document.getElementById('header').classList.add('small');
+};
+function restart(){
+    document.getElementById('play').style.display='none';
+    document.getElementById('start').style.display='flex';
+    document.getElementById('header').classList.remove('small');
+}
+function getRandomInt(max) {
+    return Math.floor(Math.random() * Math.floor(max));
+}
 
-        start = () => {
-            document.getElementById('start').style.display='none';
-            document.getElementById('play').style.display='flex';
-            document.getElementById('header').classList.add('small');
-        }
+function generate(){
+    return questions[getRandomInt(questions.length)];
+};
 
-        restart = () => {
-            document.getElementById('play').style.display='none';
-            document.getElementById('start').style.display='flex';
-            document.getElementById('header').classList.remove('small');
-        }
-        
-        getRandomInt = (max) => { 
-            let bufferContainsRandomInt = false
-            let randomInt = -1            
-
-            do {
-                randomInt = Math.floor(Math.random() * Math.floor(max));
-                bufferContainsRandomInt = this.usedQuestionsBuffer.indexOf(randomInt) >= 0
-                if (!bufferContainsRandomInt) {
-                    this.usedQuestionsBuffer.push(randomInt)
-                }
-            } while(bufferContainsRandomInt)
-
-            if (this.usedQuestionsBuffer.length > USED_QUESTIONS_BUFFER_MAX_SIZE) {
-                this.usedQuestionsBuffer = this.usedQuestionsBuffer.slice(1, this.usedQuestionsBuffer.length)
-            }
-
-            return randomInt
-        }
-        
-        generate = () => {
-            return icebreaker.Questions[this.getRandomInt(icebreaker.Questions.length)]
-        }
-        
-        reload = () => {
-            document.getElementById('questions').innerHTML = this.generate();
-        }
-    }
-
-    icebreaker.IceBreaker = new IceBreaker()
-})(window.icebreaker = window.icebreaker || {});
+function reload(){
+    document.getElementById('questions').innerHTML = generate();
+}
